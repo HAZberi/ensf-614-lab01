272e9a9afce39116711b624d6a55e496c10472f7
Hassaan Zuberi <mhazuberi@gmail.com>
2022-09-20 09:50:29 -0600

    Added student name for exercise b

--- exercise-b/lab1exe_B.cpp
+++ exercise-b/lab1exe_B.cpp
@@ -2,14 +2,14 @@
  *  lab1exe_B.cpp
  *  ENSF 614 Lab 1, exercise B
  *  Created by Mahmood Moussavi
- *  Completed by: Student Name
+ *  Completed by: Hassaan Ahmed Zuberi
  */
 
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-const double G = 9.8;   /* gravitation acceleration 9.8 m/s^2 */
+const double G = 9.8; /* gravitation acceleration 9.8 m/s^2 */
 const double PI = 3.141592654;
 
 void create_table(double v);
@@ -17,14 +17,15 @@ double Projectile_travel_time(double a, double v);
 double Projectile_travel_distance(double a, double v);
 double degree_to_radian(double d);
 
-void create_table(double v) {
+void create_table(double v)
+{
     cout << "Angle \tt \t\td \n";
     cout << "(deg) \t(sec) \t\t(m) \n";
 
-    double deg = 0.0; //angle in degrees start from 0
+    double deg = 0.0; // angle in degrees start from 0
 
-
-    while(deg <= 90.0){
+    while (deg <= 90.0)
+    {
         double deg_radian = degree_to_radian(deg);
         double time = Projectile_travel_time(deg_radian, v);
         double distance = Projectile_travel_distance(deg_radian, v);
@@ -35,36 +36,39 @@ void create_table(double v) {
     }
 }
 
-double degree_to_radian(double d) {
-    return (d * (PI / 180)); 
+double degree_to_radian(double d)
+{
+    return (d * (PI / 180));
 }
 
-double Projectile_travel_distance(double a, double v){
+double Projectile_travel_distance(double a, double v)
+{
     return (((v * v) / G) * sin(2 * a));
 }
 
-double Projectile_travel_time(double a, double v) {
+double Projectile_travel_time(double a, double v)
+{
     return (((2 * v) * sin(a)) / G);
 }
 
 int main(void)
 {
     double velocity;
-    
+
     cout << "Please enter the velocity at which the projectile is launched (m/sec): ";
     cin >> velocity;
-    
-    if(!cin)  // means if cin failed to read
+
+    if (!cin) // means if cin failed to read
     {
         cout << "Invlid input. Bye...\n";
         exit(1);
     }
-    
-    while (velocity < 0 )
+
+    while (velocity < 0)
     {
         cout << "\nplease enter a positive number for velocity: ";
         cin >> velocity;
-        if(!cin)
+        if (!cin)
         {
             cout << "Invlid input. Bye...";
             exit(1);
@@ -72,8 +76,6 @@ int main(void)
     }
 
     create_table(velocity);
-    
+
     return 0;
 }
-
-
